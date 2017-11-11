db7a1cb8b43ed6b0c9daa41fe828011d440f8697
KassabNova <CKAssabAndre@gmail.com>
2017-11-10 23:01:43 -0600

    Agregando Imagenes y método para resetear. Minas escondidas

--- UAGsweeper/WindowsFormsApp1/Canvas.cs
+++ UAGsweeper/WindowsFormsApp1/Canvas.cs
@@ -11,12 +11,12 @@ using System.Windows.Forms;
 namespace WindowsFormsApp1
 {
     public partial class Canvas : Form
-    {
+    {   //Class variables
         int xPos = 0;
         int yPos = 0;
         int row;
         int col;
-        Random rnd = new Random();
+        Random rnd = new Random();//Random object for mines
 
 
         public Canvas()
@@ -27,6 +27,7 @@ namespace WindowsFormsApp1
 
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Canvas));
             this.pnlButtons = new System.Windows.Forms.Panel();
             this.btn_reinicio = new System.Windows.Forms.Button();
             this.SuspendLayout();
@@ -35,22 +36,24 @@ namespace WindowsFormsApp1
             // 
             this.pnlButtons.Location = new System.Drawing.Point(6, 48);
             this.pnlButtons.Name = "pnlButtons";
-            this.pnlButtons.Size = new System.Drawing.Size(201, 205);
+            this.pnlButtons.Size = new System.Drawing.Size(212, 215);
             this.pnlButtons.TabIndex = 0;
             // 
             // btn_reinicio
             // 
+            this.btn_reinicio.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn_reinicio.Image = ((System.Drawing.Image)(resources.GetObject("btn_reinicio.Image")));
             this.btn_reinicio.Location = new System.Drawing.Point(92, 12);
             this.btn_reinicio.Name = "btn_reinicio";
             this.btn_reinicio.Size = new System.Drawing.Size(30, 30);
             this.btn_reinicio.TabIndex = 1;
             this.btn_reinicio.Text = "=)";
-            this.btn_reinicio.UseVisualStyleBackColor = true;
+            this.btn_reinicio.UseVisualStyleBackColor = false;
             this.btn_reinicio.Click += new System.EventHandler(this.button1_Click);
             // 
             // Canvas
             // 
-            this.ClientSize = new System.Drawing.Size(213, 265);
+            this.ClientSize = new System.Drawing.Size(222, 265);
             this.Controls.Add(this.btn_reinicio);
             this.Controls.Add(this.pnlButtons);
             this.Name = "Canvas";
@@ -62,8 +65,10 @@ namespace WindowsFormsApp1
 
         private void AddCeldas()
         {
-            
-           
+
+            //Reset X and Y Pos
+            xPos = 0;
+            yPos = 0;
             // Declare and assign number of buttons 
             System.Windows.Forms.Button[,]btnArray = new System.Windows.Forms.Button[10,10];
             // Create Buttons: 
@@ -73,36 +78,40 @@ namespace WindowsFormsApp1
                 btnArray[row,col] = new System.Windows.Forms.Button();
             }
            
-            //Draw to screen
+            //Draw to screen the buttons
             for ( row = 0;row< 10;row++)
             {
                 for (col = 0; col < 10; col++)
                 {
-                    int minaRand = rnd.Next(0,100);
+                    
                     btnArray[row,col].Tag = row + 1; // Tag of button 
                     btnArray[row,col].Width = 20; // Width of button 
                     btnArray[row,col].Height = 20; // Height of button 
-                    //btnArray[row, col].BackColor = ; // Height of button 
-                    // Location of button: 
+                    btnArray[row, col].Enabled = true;
+                    
+                    btnArray[row, col].BackColor= SystemColors.AppWorkspace ; // Color of button 
+
                     // Adding mines
-                    if (minaRand < 30)
+                    int minaRand = rnd.Next(0, 100);
+                    if (minaRand < 22)
                     {
-                        btnArray[row, col].Text = "@";
+                        btnArray[row, col].Text = "_";
+                        btnArray[row, col].ForeColor = SystemColors.ActiveCaptionText;
                     }
                     btnArray[row,col].Left = xPos;
                     btnArray[row,col].Top = yPos;
 
                     // Add buttons to a Panel: 
                     pnlButtons.Controls.Add(btnArray[row,col]); // Let panel hold the Buttons 
-                    xPos = xPos + btnArray[row,col].Width; // Left of next button 
+                    xPos = xPos + btnArray[row,col].Width +1; // Left of next button 
 
                     
-                    //Creating 
+                    //Creating  event Click handler
                     btnArray[row,col].Click += new System.EventHandler(ClickButton);
                     
                 }
                 xPos = 0;
-                yPos = yPos + btnArray[row, xPos].Height;
+                yPos = yPos + btnArray[row, xPos].Height +1;
             }
         }
 
@@ -110,19 +119,28 @@ namespace WindowsFormsApp1
         public void ClickButton(Object sender, System.EventArgs e)
         {
             Button btn = (Button)sender;
-        
-            if (btn.Text == "@")
+            btn.Enabled = false;
+           
+            if (btn.Text == "_")
             {
                 btn.Visible = true;
+                btn.Image = Image.FromFile("C:/Users/CKassab/source/repos/UAG_IEEE/UAGsweeper/mina.jpg");
                 MessageBox.Show("You clicked a bomb! Allahu-Akbar!!!!");
                 
             }
         }
 
+        //Reset and Adding Method on Smiley Button
         private void button1_Click(object sender, EventArgs e)
         {
+            ResetCeldas();
             AddCeldas();
         }
 
+        //Reset Method
+        private void ResetCeldas()
+        {
+            pnlButtons.Controls.Clear();
+        }
     }
 }
